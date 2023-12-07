e0edcdf0e22361c47666a3a0fffbfa649c69dfb3
Evgeny39rus <denver417@gmail.com>
2023-12-07 18:40:41 +0200

    firstStepSwift

---
+++ repo2Xcode/repo2Xcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  repo2Xcode
+//
+//  Created by Евгений Сушков on 07.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
